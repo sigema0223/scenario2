3e8168106406e5fd2f0af731b14194afce43bbee
Elonelymusk <arielandhika@gmail.com>
2022-03-23 14:46:35 +0000

    added a backend server to generate questions

---
+++ server/index.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors'; 
+import {PythonShell} from 'python-shell';
+
+/* run "node index.js" in the server directory. Make sure to change the openAI API key. and make shanges to the 
+relative path for the json files. 
+
+*/
+
+PythonShell.run("/Users/ariel/Scenario2/server/questions copy/generatorScript.py", null,function(err,results){
+    if (err) throw err;
+    console.log("finished")
+});
